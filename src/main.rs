63cb19aeb106a17133bb790ce645db4061a9b61a
Jan Bronicki <janbronicki@gmail.com>
2022-11-27 23:14:48 +0100

    lmao

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
+use eframe;
+use egui::plot::{Line, Plot, PlotPoints};
+
+struct MyApp {
+    label: String,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            // Example stuff:
+            label: "Hello World!".to_owned(),
+        }
+    }
+}
+
+impl MyApp {
+    /// Called once before the first frame.
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // This is also where you can customized the look at feel of egui using
+        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
+
+        Default::default()
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let Self { label } = self;
+        egui::CentralPanel::default().show(ctx, |ui| {
+            let sin: PlotPoints = (0..1000)
+                .map(|i| {
+                    let x = i as f64 * 0.01;
+                    [x, x.sin()]
+                })
+                .collect();
+            let line = Line::new(sin);
+            Plot::new("my_plot")
+                .view_aspect(2.0)
+                .show(ui, |plot_ui| plot_ui.line(line));
+
+            egui::warn_if_debug_build(ui);
+        });
+    }
+
+    // fn name(&self) -> &str {
+    //     "Ground Control Station"
+    // }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Log to stdout (if you run with `RUST_LOG=debug`).
+    tracing_subscriber::fmt::init();
+
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "eframe template",
+        native_options,
+        Box::new(|cc| Box::new(MyApp::new(cc))),
+    );
 }
